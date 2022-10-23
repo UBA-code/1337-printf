0599364ed692b0ed0f14fc0d5b52db763e180cc9
Yassine Bel Hachmi <ybel-hac@e2r10p10.1337.ma>
2022-10-23 20:43:09 +0100

    just sync

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 14:43:12 by ybel-hac          #+#    #+#             */
-/*   Updated: 2022/10/23 14:44:42 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2022/10/23 20:42:45 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 int printf(const char *format, ...)
 {
-	
+	int i;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+				flag_char();
+			else if (format[i + 1] == 's')
+				flag_arr();
+			else if (format[i + 1] == 'p')
+				flag_pointer();
+			else if (format[i + 1] == 'd')
+				flag_digit();
+			else if (format[i + 1] == 'i')
+				flag_
+		}
+	}
 }
