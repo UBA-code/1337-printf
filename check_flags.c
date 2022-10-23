34de8a558dcd06f93fcbd00fba25726b0c365798
Yassine Bel Hachmi <ybel-hac@e2r10p11.1337.ma>
2022-10-23 22:22:51 +0100

    starting

--- check_flags.c
+++ check_flags.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   check_flags.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/23 14:44:11 by ybel-hac          #+#    #+#             */
-/*   Updated: 2022/10/23 22:22:12 by ybel-hac         ###   ########.fr       */
+/*   Created: 2022/10/23 22:03:04 by ybel-hac          #+#    #+#             */
+/*   Updated: 2022/10/23 22:03:44 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-
-#include <stdarg.h>
-#include "./libft/libft.h"
-
-int ft_printf(const char *, ...)
-
-#endif
+void check_flags(char c)
+{
+	if (c == 'c')
+		flag_char();
+	else if (c == 's')
+		flag_arr();
+	else if (c == 'p')
+		flag_pointer();
+	else if (c == 'd')
+		flag_digit();
+	else if (c == 'i')
+		flag_int();
+	else if (c == 'u')
+		flag_unsigned();
+	else if (c == 'x')
+		flag_unsigned();
+	else if (c == 'x')
+		flag_num_hex();
+	else if (c == 'X')
+		flag_num_hex_upp();
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: ybel-hac <ybel-hac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 14:43:12 by ybel-hac          #+#    #+#             */
-/*   Updated: 2022/10/23 20:42:45 by ybel-hac         ###   ########.fr       */
+/*   Updated: 2022/10/23 22:19:34 by ybel-hac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
-int printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
 	int i;
 
@@ -20,17 +20,6 @@ int printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%')
-		{
-			if (format[i + 1] == 'c')
-				flag_char();
-			else if (format[i + 1] == 's')
-				flag_arr();
-			else if (format[i + 1] == 'p')
-				flag_pointer();
-			else if (format[i + 1] == 'd')
-				flag_digit();
-			else if (format[i + 1] == 'i')
-				flag_
-		}
+			check_flags(format[i + 1]);
 	}
 }
